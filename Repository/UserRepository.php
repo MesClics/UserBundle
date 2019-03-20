80aad42eea58b99dcdb185446ff05164eecdd4bb
MesClics <mesclics@hotmail.com>
2019-03-20 10:42:17 +0100

    use loadUserByUsernameOrEmail custom method in loadUserByUsername method.

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -4,16 +4,15 @@ namespace MesClics\UserBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 class UserRepository extends EntityRepository implements UserLoaderInterface{
     public function loadUserByUsername($username){
-        $qb = $this
-        ->createQueryBuilder('u')
-        ->where('u.username = :username OR u.email = :email')
-        ->setParameter('username', $username)
-        ->setParameter('email', $username);
-
-        return $qb->getQuery()->getOneOrNullResult();
+        $user = $this->loadUserByUsernameOrEmail($username);
+        if(!$user){
+            throw new UsernameNotFoundException('No user found for username or email '. $username);
+        }
+        return $user;
     }
 
     public function loadUserByUsernameOrEmail($username){
